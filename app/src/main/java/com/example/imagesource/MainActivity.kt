13bd6c32b9acdd416a326ae424616a7adb73456b
rawhasan <rawhasanweb@gmail.com>
2021-08-10 13:36:50 +0600

    show previously set image if none selected from the gallery

--- app/src/main/java/com/example/imagesource/MainActivity.kt
+++ app/src/main/java/com/example/imagesource/MainActivity.kt
@@ -13,14 +13,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 
 class MainActivity : ComponentActivity() {
     private var imageUriState = mutableStateOf<Uri?>(null)
+    private var imageSetUrl: Uri? = null
 
     private val selectImageLauncher = registerForActivityResult(GetContent()) { uri ->
+        imageSetUrl = imageUriState.value
         imageUriState.value = uri
     }
 
@@ -37,86 +38,16 @@ class MainActivity : ComponentActivity() {
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center
         ) {
-
-            // Solution - 1 by Philip (SO) - works after adding (.value)
-//            Image(
-//                painter = if (imageUriState.value != null) {
-//                    rememberImagePainter(
-//                        imageUriState.value
-//                    )
-//                } else {
-//                    painterResource(id = R.drawable.blank_profile_picture)
-//                },
-//                contentDescription = "profile image",
-//                contentScale = ContentScale.Crop,
-//                modifier = Modifier.fillMaxWidth()
-//            )
-
-
-            // Solution - 2 by Philip (SO) - Doesn't work
-//            Image(
-//                painter = rememberImagePainter(
-//                    imageUriState.value,
-//                    builder = {
-//                        placeholder(R.drawable.blank_profile_picture)
-//                    }
-//                ),
-//                contentDescription = "profile image",
-//                contentScale = ContentScale.Crop,
-//                modifier = Modifier.fillMaxWidth()
-//            )
-
-
-//            Solution by Gabriele (SO) - Doesn't work
-//            val painter = rememberImagePainter(
-//                imageUriState.value,
-//                builder = {
-//                    placeholder(R.drawable.blank_profile_picture)
-//                }
-//            )
-//            Image(
-//                painter = painter,
-//                contentDescription = "profile image",
-//                contentScale = ContentScale.Crop,
-//                modifier = Modifier.fillMaxWidth()
-//            )
-
-
-//            Bug fixed (.value): Works
-//            if (imageUriState.value != null) {
-//                Image(
-//                    painter = rememberImagePainter(imageUriState.value),
-//                    contentDescription = "Profile image",
-//                    contentScale = ContentScale.Crop,
-//                    modifier = Modifier.fillMaxWidth()
-//                )
-//
-//            } else {
-//                Image(
-//                    painter = rememberImagePainter(R.drawable.blank_profile_picture),
-//                    contentDescription = "Profile image",
-//                    contentScale = ContentScale.Crop,
-//                    modifier = Modifier.fillMaxWidth()
-//                )
-//            }
-
-
-//                if (imageUriState != null) {
-//                    painter = rememberImagePainter(imageUriState.value)
-//                } else {
-//                    painter = rememberImagePainter( R.drawable.blank_profile_picture)
-//                },
-//
-//
-
-
-            // Bug fixed: Added ".value" - Works now!
             Image(
                 painter = rememberImagePainter(
                     if (imageUriState.value != null) {
                         imageUriState.value
                     } else {
-                        R.drawable.blank_profile_picture
+                        if (imageSetUrl != null) {
+                            imageSetUrl
+                        } else {
+                            R.drawable.blank_profile_picture
+                        }
                     }
                 ),
                 contentDescription = "profile image",
@@ -143,12 +74,7 @@ class MainActivity : ComponentActivity() {
                 }
 
             }
+
         }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    //ImageSourceActivityScreen()
 }
